35a9e7e99ce980bca6c25839d6479e49a30afa8a
vijay107 <vijayparadhi107@gmail.com>
2020-07-30 11:39:10 +0530

    Updates

---
+++ precache-manifest.080e998acf595c16a8c8b494c5e16f9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce519c7bc65a82bdf411ca621bf17ce",
+    "url": "/reactappcards/index.html"
+  },
+  {
+    "revision": "25475b081d1131b73aea",
+    "url": "/reactappcards/static/css/main.02cfbee5.chunk.css"
+  },
+  {
+    "revision": "1b7915dda5eaf32bf240",
+    "url": "/reactappcards/static/js/2.6b376481.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactappcards/static/js/2.6b376481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25475b081d1131b73aea",
+    "url": "/reactappcards/static/js/main.824cbab2.chunk.js"
+  },
+  {
+    "revision": "ee60dfb286d324098f59",
+    "url": "/reactappcards/static/js/runtime-main.3f9fcf3d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactappcards/static/media/logo.5d5d9eef.svg"
+  }
+]);
